af43e24da2f19653233950c71c952750dfff4eb2
SK <sk@gmail.com>
2021-11-23 22:19:27 +1000

    react-essentials project added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg'
 
-function App() {
+const dishes = [ "Pizza", "Pasta", "Burgers"];
+const dishMap =  dishes.map((dish, i) => (  {key:i , val: dish }));
+const [furnitureList] = [ "Tables", "Chairs", "Bar Stool"];
+
+function Header(props) {
+  return (<header><h1>{props.name} {props.info} Kitchen</h1></header>)
+}
+
+function Main(props) {
+  
   return (
+    <section>
+      <img src="https://hota.com.au/wp-content/uploads/2021/05/Untitled-design-10.png" height="200"/>
+      <ul>
+        {props.dishMap.map((dish) =>  <li className="leftAlign" key={dish.key}>{dish.val}</li>)}
+      </ul>
+    </section>)
+}
+
+function Footer(props) {
+  return (<footer><h5>Copyright {props.year}</h5></footer>)
+}
+
+function App(props) {
+  return props.authorised ? secretComponent() : genericComponent() ;
+}
+
+function myResaurantContent() {
+   return (    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Sushant's" info="First" />
+      <Main dishMap={dishMap} />
+      <Footer year={new Date().getFullYear()} />
     </div>
-  );
+   )
+}
+
+function secretComponent() {
+  return <h1>Only secret users can see this</h1>
+}
+
+function genericComponent() {
+  return <h1>Everyone can see this</h1>
 }
 
 export default App;
